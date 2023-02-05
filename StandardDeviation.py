84891566bfb9ab9da7ce5ffd09bd513bf74fe631
Daniel Tremura <tremuradaniel@gmail.com>
2023-02-05 19:00:32 +0200

    standard deviation

--- StandardDeviation.py
+++ StandardDeviation.py
@@ -2,30 +2,57 @@ class StandardDeviation:
   ROUND_DECIMALS = 2
   
   sample = []
+  isPopulation = False
+  isVerbose = False
   
-  def __init__(self, sample) -> None:
+  def __init__(self, sample, isPopulation = False, isVerbose = False) -> None:
     self.sample = sample
+    self.isPopulation = isPopulation
+    self.isVerbose = isVerbose
+    self._setMean()
+    self._setVariance()
+    self._setStandardDeviation()
+  
+  def getMean(self):
+    return self.mean
+  
+  def getStandardDeviation(self):
+    return self.standarDeviation
+  
+  def getVariation(self):
+    return self.variation
 
-  def getUnbiasedSampleDeviation(self):
-    """ return the unbiased sample deviation for a given sample with which the class
+  def _setVariance(self):
+    """ 
+      return the unbiased sample deviation for a given sample with which the class
       was initialized
         Variance is the measure of how far the data is spread from the mean
-
-    Returns:
-        _type_: float
     """
-    mean = self.getMean()
-        
+    
+    sampleSize = len(self.sample) if self.isPopulation else len(self.sample) - 1
+            
     squared_deviations = 0
     
     for s in self.sample:
-      squared_deviations += (s - mean)**2
-      print("deviation {}", (s - mean)**2)
-      
-    print("squared_deviations {}", squared_deviations / 9)
-      
-    return round((squared_deviations / (len(self.sample) - 1)), self.ROUND_DECIMALS)
-      
+      squared_deviations += (s - self.getMean())**2
+      if self.isVerbose:
+        print("squared_deviations - ", (s - self.getMean())**2)
     
-  def getMean(self):
-    return sum(self.sample) / len(self.sample)
+    self.variation = round((squared_deviations / sampleSize), self.ROUND_DECIMALS)
+    
+    if self.isVerbose:
+      print("Variation: %f" % (self.variation))
+
+  def _setStandardDeviation(self):
+    self.standarDeviation = round(self.variation**0.5, self.ROUND_DECIMALS) # square root the unbiased deviation
+    
+    if self.isVerbose:
+      standardDeviationType = "Population" if self.isPopulation else "Sample"
+      print("Standard %s Deviation: %f" % (standardDeviationType, self.standarDeviation))
+      
+  
+  def _setMean(self):
+    self.mean = sum(self.sample) / len(self.sample)
+    
+    if self.isVerbose:
+      print("Mean ", self.mean)
--- main.py
+++ main.py
@@ -2,8 +2,4 @@ import StandardDeviation
 
 ages = [43, 35, 38, 56, 29, 33, 46, 63, 49, 40]
 
-sd = StandardDeviation.StandardDeviation(ages)
-
-
-print(sd.getUnbiasedSampleDeviation())
-print(sd.getMean())
+sd = StandardDeviation.StandardDeviation(ages, False, True)
