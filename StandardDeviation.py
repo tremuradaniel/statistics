98883b2ad22dcc77a11d5b5c32eb33e691165e04
Daniel Tremura <tremuradaniel@gmail.com>
2023-02-05 17:50:00 +0200

    refactored

--- StandardDeviation.py
+++ StandardDeviation.py
@@ -1,27 +1,31 @@
 class StandardDeviation: 
   ROUND_DECIMALS = 2
+  
+  sample = []
+  
+  def __init__(self, sample) -> None:
+    self.sample = sample
 
-  def getUnbiasedSampleDeviation(self, sample):
-    """ return the unbiased sample deviation for a given sample
+  def getUnbiasedSampleDeviation(self):
+    """ return the unbiased sample deviation for a given sample with which the class
+      was initialized
         Variance is the measure of how far the data is spread from the mean
-    Args:
-        sample (list): sample records
 
     Returns:
         _type_: float
     """
-    mean = self._getMean(sample)
+    mean = self.getMean()
         
     squared_deviations = 0
     
-    for s in sample:
+    for s in self.sample:
       squared_deviations += (s - mean)**2
       print("deviation {}", (s - mean)**2)
       
     print("squared_deviations {}", squared_deviations / 9)
       
-    return round((squared_deviations / (len(sample) - 1)), self.ROUND_DECIMALS)
+    return round((squared_deviations / (len(self.sample) - 1)), self.ROUND_DECIMALS)
       
     
-  def _getMean(self, sample):
-    return sum(sample) / len(sample)
+  def getMean(self):
+    return sum(self.sample) / len(self.sample)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import StandardDeviation
 
-usv = StandardDeviation.StandardDeviation()
-
 ages = [43, 35, 38, 56, 29, 33, 46, 63, 49, 40]
 
-print(usv.getUnbiasedSampleDeviation(ages))
+sd = StandardDeviation.StandardDeviation(ages)
+
+
+print(sd.getUnbiasedSampleDeviation())
+print(sd.getMean())
